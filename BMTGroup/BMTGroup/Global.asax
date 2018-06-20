2f1546b252d7483e1d01657b476dd5ec34c2e32c
Iliyan Stoyanov <virus_22@abv.bg>
2018-06-20 16:57:10 +0300

    Initial Commit - added simple JS to the home page

---
+++ BMTGroup/BMTGroup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BMTGroup.MvcApplication" Language="C#" %>
---
+++ BMTGroup/BMTGroup/Scripts/navigation.js
@@ -0,0 +1,13 @@
+﻿//window.location.hash = "jump_to_this_location";
+//$(document).ready(function () {
+//    $(".navigateContact").click(function () {
+//        window.location.hash = "contact";
+//});
+
+$("nav").find("a").click(function (e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
